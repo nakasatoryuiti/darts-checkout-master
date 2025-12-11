d70afb9f0023e2616913ccbf014aae9dfae4d6cb
homug <homugutamu@gmail.com>
2025-12-11 13:15:18 +0900

    Clean: Removed base option as gh-pages is now used for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     
     return {
       // 🚨 baseを直書きすることで、確実にパスを設定 🚨
-      base: '/darts-checkout-master/', 
+      // base: '/darts-checkout-master/', 
       
       server: {
         port: 3000,
